ad72d7a61bf5a2798de3ffbb183ec398e33e9ec2
Syed Asifullah <syedasifullah00@gmail.com>
2024-10-03 18:37:09 +0500

    header btn update

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -27,7 +27,7 @@ export const Header = () => {
               <a href="#">Carbon Footprint</a>
               <a href="#">Updates</a>
               <a href="#">Help</a>
-              <button className="">get for free</button>
+              <button className="btn btn-primary">get for free</button>
             </nav>
           </div>
         </div>
